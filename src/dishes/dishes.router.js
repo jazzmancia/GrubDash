ad91583cfe8b808edd93fd39d0bce234fcef9370
Jazz <86076004+jazzmancia@users.noreply.github.com>
2021-10-27 13:31:34 -0700

    remove TODO comments

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-// TODO: Implement the /dishes routes needed to make the tests pass
 
 router
     .route("/:dishId")
